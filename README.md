5245f61ccc1c9c544006fd610197e5cb0f78eecb
Adwaver4157 <adwaver4157@gmail.com>
2024-10-09 17:21:51 +0900

    :tada: Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # IsaacLabExtendedTasks
+This repository is based on IsaacLab and is designed to add new tasks that are not available in `omni.isaac.lab_tasks`.
+
+## Structure
+The repository, IsaacLabExtendedTasks, consists of two main components:
+- `omni.isaac.extended_assets`
+- `omni.isaac.extended_tasks`
+
+Both components are built on top of `omni.isaac.lab_assets` and `omni.isaac.lab_tasks`.
+
+## Installation
+To install this repository, follow these steps:
+
+1. Clone the repository.
+```
+git clone https://github.com/makolon/IsaacLabExtendedTasks.git
+```
+
+2. Run the following command in each directory to install the necessary components:
+```
+cd omni.isaac.extended_assets
+pip install -e .
+
+cd omni.isaac.extended_tasks
+pip install -e .
+```
+
+## Usage
+This repository is intended for use with IsaacTAMP and IsaacLab. It can be installed via the following command:
+```
+pip install git+https://github.com/makolon/IsaacLabExtendedTasks.git/#subdirectory=omni.isaac.extended_assets
+pip install git+https://github.com/makolon/IsaacLabExtendedAssets.git/#subdirectory=omni.isaac.extended_tasks
+```
+
+For detailed usage instructions, please refer to the README files located in each directory.
---
+++ scripts/create_scene.py
@@ -0,0 +1,71 @@
+import argparse
+
+from omni.isaac.lab.app import AppLauncher
+
+# Add argparse arguments
+parser = argparse.ArgumentParser(
+    description="Sample code for demonstrating IsaacLabExtendedTasks."
+)
+parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument(
+    "--num_envs", type=int, default=1, help="Number of environments to spawn."
+)
+# Append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# Parse the arguments
+args_cli = parser.parse_args()
+
+# Launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import gymnasium as gym
+import omni.isaac.extended_assets  # noqa: F401
+import omni.isaac.extended_tasks  # noqa: F401
+import torch
+from omni.isaac.lab_tasks.utils import parse_env_cfg
+
+
+def main():
+    """Main function."""
+    # Create environment configuration
+    env_cfg = parse_env_cfg(
+        task_name=args_cli.task, device=args_cli.device, num_envs=args_cli.num_envs
+    )
+    env = gym.make(args_cli.task, cfg=env_cfg)
+
+    # print info (this is vectorized environment)
+    print(f"[INFO]: Gym observation space: {env.observation_space}")
+    print(f"[INFO]: Gym action space: {env.action_space}")
+
+    # Reset environment
+    env.reset()
+
+    # Simulate physics
+    count = 0
+    while simulation_app.is_running():
+        with torch.inference_mode():
+            # Reset
+            if count % 300 == 0:
+                count = 0
+                env.reset()
+                print("-" * 80)
+                print("[INFO]: Resetting environment...")
+            # Sample random actions
+            joint_efforts = torch.randn_like(env.action_manager.action)
+            # Step the environment
+            obs, rew, terminated, truncated, info = env.step(joint_efforts)
+            # Update counter
+            count += 1
+
+    # Close the environment
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # Close sim app
+    simulation_app.close()
