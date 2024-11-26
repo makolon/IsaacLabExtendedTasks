473334c037d07d629b5d2d1f72f0d4700f236580
makolon <makoto.sato@weblab.t.u-tokyo.ac.jp>
2024-11-26 14:11:10 +0900

    Fix sorting

--- exts/omni.isaac.extended_tasks/omni/isaac/extended_tasks/sorting/config/kinova/jaco7n_env_cfg.py
+++ exts/omni.isaac.extended_tasks/omni/isaac/extended_tasks/sorting/config/kinova/jaco7n_env_cfg.py
@@ -1,21 +1,21 @@
 import os
 import random
-from omni.isaac.extended_tasks.assembly import mdp
-from omni.isaac.extended_tasks.assembly.assembly_env_cfg import AssemblyEnvCfg
+from omni.isaac.extended_tasks.sorting import mdp
+from omni.isaac.extended_tasks.sorting.sorting_env_cfg import SortingEnvCfg
 from omni.isaac.lab.assets import RigidObjectCfg
 from omni.isaac.lab.sensors import FrameTransformerCfg
 from omni.isaac.lab.sensors.frame_transformer.frame_transformer_cfg import OffsetCfg
 from omni.isaac.lab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
 from omni.isaac.lab.sim.spawners.from_files.from_files_cfg import UsdFileCfg
 from omni.isaac.lab.utils import configclass
+from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR
 
 from omni.isaac.lab.markers.config import FRAME_MARKER_CFG  # isort: skip
 from omni.isaac.extended_assets.kinova import KINOVA_JACO_7N_CFG  # isort: skip
-from omni.isaac.extended_tasks.assembly import FUSION360_FIXTURE_PATH, FUSION360_OBJECT_PATH  # isort: skip
 
 
 @configclass
-class Jaco7NAssemblyEnvCfg(AssemblyEnvCfg):
+class Jaco7NSortingEnvCfg(SortingEnvCfg):
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
@@ -40,66 +40,39 @@ class Jaco7NAssemblyEnvCfg(AssemblyEnvCfg):
         # Set the body name for the end effector
         self.commands.object_pose.body_name = "j2n7n300_end_effector"
 
-        # Create dictionaries with IDs as keys and paths as values
-        fixture_dict = {os.path.basename(os.path.dirname(os.path.dirname(fixture))): fixture for fixture in FUSION360_FIXTURE_PATH}
-        object_dict = {os.path.basename(os.path.dirname(os.path.dirname(obj))): obj for obj in FUSION360_OBJECT_PATH}
-
-        # Find common IDs and corresponding paths
-        common_ids = set(fixture_dict.keys()) & set(object_dict.keys())
-        common_paths = [(fixture_dict[id], object_dict[id]) for id in common_ids]
-
-        # Select parts path randomly
-        fixture_path, object_path = random.choice(common_paths)
-
-        # Set target object
-        self.scene.object = RigidObjectCfg(
-            prim_path="{ENV_REGEX_NS}/Object",
-            init_state=RigidObjectCfg.InitialStateCfg(
-                pos=[0.5, 0, 0.055], rot=[1, 0, 0, 0]
-            ),
-            spawn=UsdFileCfg(
-                usd_path=object_path,
-                scale=(1.0, 1.0, 1.0),
-                rigid_props=RigidBodyPropertiesCfg(
-                    solver_position_iteration_count=16,
-                    solver_velocity_iteration_count=0,
-                    max_angular_velocity=64.0,
-                    max_linear_velocity=1000.0,
-                    max_depenetration_velocity=5.0,
-                    linear_damping=0.5,
-                    angular_damping=0.5,
-                    enable_gyroscopic_forces=True,
-                    rigid_body_enabled=True,
-                    disable_gravity=False,
+        # Collect all configs
+        block_cfgs = {}
+        for block_color in ["blue", "green", "red", "yellow"]:
+            block_cfgs[block_color] = RigidObjectCfg(
+                prim_path="{ENV_REGEX_NS}/" + "{}Block".format(block_color.capitalize()),
+                init_state=RigidObjectCfg.InitialStateCfg(
+                    pos=[0.5, 0, 0.02], rot=[1, 0, 0, 0]
                 ),
-            ),
-        )
-
-        # Set fixture
-        self.scene.fixture = RigidObjectCfg(
-            prim_path="{ENV_REGEX_NS}/Fixture",
-            init_state=RigidObjectCfg.InitialStateCfg(
-                pos=[0.5, 0.1, 0.055], rot=[1, 0, 0, 0]
-            ),
-            spawn=UsdFileCfg(
-                usd_path=fixture_path,
-                scale=(1.0, 1.0, 1.0),
-                rigid_props=RigidBodyPropertiesCfg(
-                    solver_position_iteration_count=16,
-                    solver_velocity_iteration_count=0,
-                    max_angular_velocity=64.0,
-                    max_linear_velocity=1000.0,
-                    max_depenetration_velocity=5.0,
-                    linear_damping=0.5,
-                    angular_damping=0.5,
-                    enable_gyroscopic_forces=True,
-                    rigid_body_enabled=True,
-                    disable_gravity=False,
-                    kinematic_enabled=True,
+                spawn=UsdFileCfg(
+                    usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/{block_color}_block.usd",
+                    scale=(1.0, 1.0, 1.0),
+                    rigid_props=RigidBodyPropertiesCfg(
+                        solver_position_iteration_count=16,
+                        solver_velocity_iteration_count=0,
+                        max_angular_velocity=64.0,
+                        max_linear_velocity=1000.0,
+                        max_depenetration_velocity=5.0,
+                        linear_damping=0.5,
+                        angular_damping=0.5,
+                        enable_gyroscopic_forces=True,
+                        rigid_body_enabled=True,
+                        disable_gravity=False,
+                    ),
                 ),
-            ),
-        )
+            )
 
+        # Set the configs as member of the class
+        for k, v in block_cfgs.items():
+            attr_name = "{}_block".format(k)
+            self.scene.__setattr__(attr_name, v)
+
+        # Set target object (randomly)
+        self.scene.target_object = random.choice(list(block_cfgs.values()))
 
         # Listens to the required transforms
         marker_cfg = FRAME_MARKER_CFG.copy()
@@ -122,7 +95,7 @@ class Jaco7NAssemblyEnvCfg(AssemblyEnvCfg):
 
 
 @configclass
-class Jaco7NAssemblyEnvCfg_PLAY(Jaco7NAssemblyEnvCfg):
+class Jaco7NSortingEnvCfg_PLAY(Jaco7NSortingEnvCfg):
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
