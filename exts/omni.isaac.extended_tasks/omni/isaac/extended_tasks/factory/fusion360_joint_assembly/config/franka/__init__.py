fa0b0cf31ae90ff5a199d7910d4377baef1d9ac1
makolon <makoto.sato@weblab.t.u-tokyo.ac.jp>
2025-01-06 01:12:50 +0900

    Fix task name

--- exts/omni.isaac.extended_tasks/omni/isaac/extended_tasks/factory/fusion360_joint_assembly/config/franka/__init__.py
+++ exts/omni.isaac.extended_tasks/omni/isaac/extended_tasks/factory/fusion360_joint_assembly/config/franka/__init__.py
@@ -3,7 +3,7 @@ import gymnasium as gym
 from . import agents, franka_env_cfg
 
 gym.register(
-    id="Isaac-Assembly-Franka-v0",
+    id="Isaac-Fusion360-Joint-Franka-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": franka_env_cfg.FrankaAssemblyEnvCfg,
